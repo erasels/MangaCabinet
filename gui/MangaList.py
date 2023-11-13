789916ac3278b34db54a3fa50654117b08d08fc1
erasels <emanuelbedburdick@yahoo.de>
2023-11-13 20:22:40 +0100

    Optimize background drawing logic, add score, background and removed drawing for thumbnail delegate

--- gui/MangaList.py
+++ gui/MangaList.py
@@ -202,19 +202,19 @@ class MangaDelegate(QStyledItemDelegate):
 
         background_color = DEFAULT_ITEM_BG_COLOR
 
-        # Set group-specific color
-        group_name = entry.group
-        if group_name and group_name in self.mw.group_handler.groups:
-            color = self.mw.group_handler.groups[group_name].get("color")
-            if color:
-                background_color = blend_colors(DEFAULT_ITEM_BG_COLOR, QColor(color), 0.2)
-
-        if option.state & QStyle.State_MouseOver:
-            mod_color = QColor(255, 255, 255, 50)  # semi-transparent white to brighten the color
-            background_color = blend_colors(background_color, mod_color, 0.8)
-
         if self.mw.details_handler.cur_data and self.mw.details_handler.cur_data.id == entry.id:
             background_color = option.palette.highlight().color()
+        else:
+            # Set group-specific color
+            group_name = entry.group
+            if group_name and group_name in self.mw.group_handler.groups:
+                color = self.mw.group_handler.groups[group_name].get("color")
+                if color:
+                    background_color = blend_colors(DEFAULT_ITEM_BG_COLOR, QColor(color), 0.2)
+
+            if option.state & QStyle.State_MouseOver:
+                mod_color = QColor(255, 255, 255, 50)  # semi-transparent white to brighten the color
+                background_color = blend_colors(background_color, mod_color, 0.8)
 
         item_path = QPainterPath()
         item_path.addRoundedRect(QRectF(option.rect), 5, 5)
@@ -527,6 +527,11 @@ class ThumbnailDelegate(QStyledItemDelegate):
         super(ThumbnailDelegate, self).__init__(parent, *args, **kwargs)
         self.mw = mw
         self.cache = {}
+        self.img_star = QPixmap(os.path.join(self.mw.image_path, 'star.png'))
+        self.img_star = self.img_star.scaled(int(self.img_star.width() * 0.5),
+                                             int(self.img_star.height() * 0.5),
+                                             Qt.KeepAspectRatio)
+
         self.threadPool = QThreadPool()
         self.imageLoader = ImageLoader(mw.thumbnail_manager)
         self.imageLoader.imageLoaded.connect(self.onImageLoaded)
@@ -537,12 +542,43 @@ class ThumbnailDelegate(QStyledItemDelegate):
         self.parent().viewport().update()  # Update the view to redraw items with the loaded image
 
     def paint(self, painter, option, index):
-        painter.save()
-
-        # Retrieve item data
+        # Retrieve item data from the model
         entry = index.data(Qt.UserRole)
         thumbnail = self.mw.thumbnail_manager.get_thumbnail(entry.id)
         title = entry.display_title()
+
+        # Draw the background and border
+        painter.save()
+
+        # Reduce background opacity if removed
+        if entry.removed:
+            painter.setOpacity(0.2)
+
+        background_color = DEFAULT_ITEM_BG_COLOR
+
+        if self.mw.details_handler.cur_data and self.mw.details_handler.cur_data.id == entry.id:
+            background_color = option.palette.highlight().color()
+        else:
+            # Set group-specific color
+            group_name = entry.group
+            if group_name and group_name in self.mw.group_handler.groups:
+                color = self.mw.group_handler.groups[group_name].get("color")
+                if color:
+                    background_color = blend_colors(DEFAULT_ITEM_BG_COLOR, QColor(color), 0.2)
+
+            if option.state & QStyle.State_MouseOver:
+                mod_color = QColor(255, 255, 255, 50)  # semi-transparent white to brighten the color
+                background_color = blend_colors(background_color, mod_color, 0.8)
+
+        item_path = QPainterPath()
+        item_path.addRoundedRect(QRectF(option.rect), 5, 5)
+        painter.fillPath(item_path, background_color)
+        painter.strokePath(item_path, QPen(QColor("#666666"), 1))
+        painter.restore()
+
+        painter.save()
+        if entry.removed:
+            painter.setOpacity(0.2)
 
         # Scale the thumbnail, maintaining aspect ratio
         max_thumb_height = option.rect.height()  # Use full height for the thumbnail
@@ -553,6 +589,9 @@ class ThumbnailDelegate(QStyledItemDelegate):
         thumb_y = option.rect.top() + (option.rect.height() - scaled_thumb.height()) / 2  # Center vertically
         thumb_rect = QRect(int(thumb_x), int(thumb_y), int(scaled_thumb.width()), int(scaled_thumb.height()))
         painter.drawPixmap(thumb_rect, scaled_thumb)
+
+        painter.restore()
+        painter.save()
 
         # Determine if the title needs to be split into two lines
         font_metrics = QFontMetrics(painter.font())
@@ -596,6 +635,31 @@ class ThumbnailDelegate(QStyledItemDelegate):
             painter.drawText(second_line_rect, Qt.AlignCenter, title_line_2)
         else:
             painter.drawText(title_background_rect, Qt.AlignCenter, title_line_1)
+
+        score = entry.score
+        if score:
+            star_spacing = 4  # adjust this based on your preferences
+            star_width = self.img_star.width()
+            star_height = self.img_star.height()
+            total_height_for_stars = score * star_height + (score - 1) * star_spacing
+
+            # Calculating the top-left point to start drawing stars
+            start_x = option.rect.x() + 5  # adding 8 pixels padding from left. Adjust as needed.
+            start_y = option.rect.y() + 5  # center align vertically
+
+            rect_width = star_width + 8
+            rect_height = score * star_height + (score - 1) * star_spacing + 10
+            rect_color = blend_colors(background_color, STAR_DIM_BG_COLOR, 0.7)
+            rect_color.setAlpha(180)
+            painter.save()
+            painter.setBrush(rect_color)
+            painter.setPen(Qt.NoPen)
+            # the 5,5 are the x,y radii of the rounded corners
+            painter.drawRoundedRect(QRect(start_x - 4, start_y - 3, rect_width, rect_height), 5, 5)
+            painter.restore()
+
+            for i in range(score):
+                painter.drawPixmap(start_x, int(start_y + i * (star_height + star_spacing)), self.img_star)
 
         painter.restore()
 
