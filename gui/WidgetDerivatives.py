626cfc1d1991922795b6aa12f1ac5b677fac2f8d
erasels <emanuelbedburdick@yahoo.de>
2024-10-03 15:29:50 +0200

    Overhaults look of tag widget, using custom flow layout and changing up the look

--- gui/WidgetDerivatives.py
+++ gui/WidgetDerivatives.py
@@ -3,11 +3,11 @@ import os
 import re
 
 from PyQt5 import QtCore
-from PyQt5.QtCore import pyqtSignal, Qt, QRectF, QPointF, QPoint, pyqtSlot, QTimer, QPropertyAnimation
+from PyQt5.QtCore import pyqtSignal, Qt, QRectF, QPointF, QPoint, pyqtSlot, QTimer, QPropertyAnimation, QSize, QRect
 from PyQt5.QtGui import QColor, QPainter, QPixmap, QWheelEvent, QMouseEvent, QShowEvent, QHideEvent, QCursor
 from PyQt5.QtWidgets import QComboBox, QCompleter, QTextEdit, QVBoxLayout, QWidget, QLineEdit, QListWidget, QLabel, \
     QListWidgetItem, QGridLayout, QScrollArea, QPushButton, QInputDialog, QListView, QGraphicsView, QGraphicsScene, \
-    QHBoxLayout, QGraphicsDropShadowEffect, QMenu, QAction, QTableWidget, QTableWidgetItem, QMessageBox, QHeaderView, QStyledItemDelegate
+    QHBoxLayout, QGraphicsDropShadowEffect, QMenu, QAction, QTableWidget, QTableWidgetItem, QMessageBox, QHeaderView, QStyledItemDelegate, QSizePolicy, QLayout
 
 import gui
 from auxillary.DataAccess import MangaEntry
@@ -341,12 +341,11 @@ class TagsWidget(QWidget):
         super(TagsWidget, self).__init__(parent)
 
         self.mw = mw
-        self.current_row, self.current_col = 0, 0
         self.loaded = False
 
+        # Create flow layout
         self.tags_widget = QWidget(self)
-        self.tags_layout = QGridLayout(self.tags_widget)
-        self.tags_layout.setAlignment(Qt.AlignTop)
+        self.tags_layout = FlowLayout(self.tags_widget)
 
         self.scroll_area = QScrollArea(self)
         self.scroll_area.setWidget(self.tags_widget)
@@ -371,22 +370,15 @@ class TagsWidget(QWidget):
     def emit_save_signal(self):
         self.saveSignal.emit()
 
-    def add_tag_to_layout(self, tag_name, row, col):
-        # Create the QPushButton with both the tag name and the '❌' symbol
-        tag_btn = QPushButton(f"❌ {tag_name}")
+    def add_tag_to_layout(self, tag_name):
+        # Create the Button with both the tag name and the '❌' symbol
+        tag_btn = QPushButton(f"❌{tag_name}")
         tag_btn.setStyleSheet(self.mw.styles.get("tagbutton"))
         tag_btn.setObjectName("Unclicked")  # This allows us to use custom selectors
         tag_btn.clicked.connect(lambda: self.tag_clicked(tag_btn, tag_name))
         tag_btn.setProperty("greyed_out", False)
 
-        # Add the button to the layout
-        self.tags_layout.addWidget(tag_btn, row, col)
-
-        # Update the current_row and current_col values
-        self.current_col += 1
-        if self.current_col > 1:
-            self.current_col = 0
-            self.current_row += 1
+        self.tags_layout.addWidget(tag_btn)
 
     def tag_clicked(self, tag_btn, original_text):
         # Handles graying out which will be used by save_changes to remove it later on
@@ -428,7 +420,7 @@ class TagsWidget(QWidget):
             modified = False
             for tag in tags:
                 if tag and tag not in existing_tags:
-                    self.add_tag_to_layout(tag, self.current_row, self.current_col)
+                    self.add_tag_to_layout(tag)
                     modified = True
             if modified:
                 self.emit_save_signal()
@@ -441,20 +433,17 @@ class TagsWidget(QWidget):
                 is_greyed_out = widget.property("greyed_out")
                 # If the button isn't greyed out, extract its tag text
                 if not is_greyed_out:
-                    tag_text = widget.text().replace("❌ ", "")
+                    tag_text = widget.text().replace("❌", "")
                     tags.append(tag_text)
         return tags
 
     def load_tags(self, tags_list):
         self.loaded = True
         self.clear_tags_layout()
-        self.current_row, self.current_col = 0, 0
         for tag in tags_list:
-            self.add_tag_to_layout(tag, self.current_row, self.current_col)
+            self.add_tag_to_layout(tag)
 
     def clear_tags_layout(self):
-        self.current_row = 0
-        self.current_col = 0
         for i in reversed(range(self.tags_layout.count())):
             widget = self.tags_layout.itemAt(i).widget()
             if widget is not None:
@@ -462,6 +451,87 @@ class TagsWidget(QWidget):
                 self.tags_layout.removeWidget(widget)
                 # Destroy the widget (this will also remove it from the screen)
                 widget.deleteLater()
+
+
+class FlowLayout(QLayout):
+    """
+    Special layout that allows adding items of different sizes to it and fits as many items into
+    a row as there is space before moving going to the next.
+    """
+    def __init__(self, parent=None, margin=0, spacing=-1):
+        super(FlowLayout, self).__init__(parent)
+        self.setContentsMargins(margin, margin, margin, margin)
+        self.setSpacing(spacing)
+
+        self.itemList = []
+
+    def addItem(self, item):
+        self.itemList.append(item)
+
+    def count(self):
+        return len(self.itemList)
+
+    def itemAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList[index]
+        return None
+
+    def takeAt(self, index):
+        if 0 <= index < len(self.itemList):
+            return self.itemList.pop(index)
+        return None
+
+    def expandingDirections(self):
+        return Qt.Orientations(Qt.Orientation(0))
+
+    def hasHeightForWidth(self):
+        return True
+
+    def heightForWidth(self, width):
+        return self.doLayout(QRect(0, 0, width, 0), True)
+
+    def setGeometry(self, rect):
+        super(FlowLayout, self).setGeometry(rect)
+        self.doLayout(rect, False)
+
+    def sizeHint(self):
+        return self.minimumSize()
+
+    def minimumSize(self):
+        size = QSize()
+        for item in self.itemList:
+            size = size.expandedTo(item.minimumSize())
+        size += QSize(2 * self.contentsMargins().left(), 2 * self.contentsMargins().top())
+        return size
+
+    def doLayout(self, rect, testOnly):
+        x = rect.x()
+        y = rect.y()
+        lineHeight = 0
+
+        for item in self.itemList:
+            wid = item.widget()
+            spaceX = self.spacing() + item.widget().style().layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Horizontal
+            )
+            spaceY = self.spacing() + item.widget().style().layoutSpacing(
+                QSizePolicy.PushButton, QSizePolicy.PushButton, Qt.Vertical
+            )
+
+            nextX = x + item.sizeHint().width() + spaceX
+            if nextX - spaceX > rect.right() and lineHeight > 0:
+                x = rect.x()
+                y = y + lineHeight + spaceY
+                nextX = x + item.sizeHint().width() + spaceX
+                lineHeight = 0
+
+            if not testOnly:
+                item.setGeometry(QRect(QPoint(x, y), item.sizeHint()))
+
+            x = nextX
+            lineHeight = max(lineHeight, item.sizeHint().height())
+
+        return y + lineHeight - rect.y()
 
 
 class ImageViewer(QGraphicsView):
